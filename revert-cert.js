054070351473f553b5999e382a1807ec890d04f9
Ramtin Soltani <ramtinsoltani93@gmail.com>
2021-01-15 18:31:52 -0800

    Updated the revert-cert script to write the cert file back after rollback.

--- revert-cert.js
+++ revert-cert.js
@@ -5,6 +5,8 @@
  * `npm run deploy` and only when all work is commited.
 */
 const child = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 // Get last commit ID
 child.exec('git rev-parse HEAD', { windowsHide: true }, (error, stdout) => {
@@ -13,12 +15,18 @@ child.exec('git rev-parse HEAD', { windowsHide: true }, (error, stdout) => {
 
   const lastCommitId = stdout.trim();
 
+  // Load certificate into memory
+  const cert = require(path.resolve(__dirname, 'firebase.cert.json'));
+
   // Rollback
   child.exec(`git reset --hard ${lastCommitId}`, { windowsHide: true }, (error, stdout) => {
 
     if ( error ) return console.error(error);
 
     console.log('Hard reset done');
+
+    // Write the cert file back
+    fs.writeFileSync(path.resolve(__dirname, 'firebase.cert.json'), cert, { encoding: 'utf-8' });
 
   });
 
